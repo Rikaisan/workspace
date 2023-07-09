e78e956419f447de4616b9766382bbf294ab00b6
Rikai <rikai@rikaisan.com>
2023-07-09 06:22:45 -0500

    Fix locale new line

--- scripts/setup/setup.sh
+++ scripts/setup/setup.sh
@@ -24,9 +24,9 @@ then
     echo "$PREFIX Setting up locales and time..."
     ln -sf /usr/share/zoneinfo/America/Bogota /etc/localtime
     hwclock --systohc
-    echo "en_US.UTF-8 UTF-8\nen_GB.UTF-8 UTF-8" > /etc/locale.gen
+    printf "en_US.UTF-8 UTF-8\nen_GB.UTF-8 UTF-8" > /etc/locale.gen
     locale-gen
-    echo "LANG=en_US.UTF-8\nLC_TIME=en_GB.UTF-8" >> /etc/locale.conf
+    printf "LANG=en_US.UTF-8\nLC_TIME=en_GB.UTF-8" > /etc/locale.conf
 
     read -p "$INPUT_PREFIX Type your desired hostname: " -r
     echo $RESULT >> /etc/hostname
