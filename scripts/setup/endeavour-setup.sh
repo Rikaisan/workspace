2710bd8030d76b8da5002c48cdeb030a8c0f487d
Rikai <rikaisensei@gmail.com>
2023-07-31 01:59:15 -0500

    Add BSPWM and EndeavourOS files

---
+++ scripts/setup/endeavour-setup.sh
@@ -0,0 +1,17 @@
+pacman -S kitty picom zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting
+
+if !(grep -q ZDOTDIR /etc/zsh/zshenv 2> /dev/null)
+then
+echo "$WARN_PREFIX Didn't find ZDOTDIR, exporting to /etc/zsh/zshenv..."
+echo export ZDOTDIR=/home/$USERNAME/.config/zsh >> /etc/zsh/zshenv
+fi
+
+if !(grep -q stow /etc/zsh/zshenv 2> /dev/null)
+then
+echo "$WARN_PREFIX Didn't find dotcfg(stow) alias, exporting to /etc/zsh/zshenv..."
+echo alias dotcfg="stow -t ~" >> /etc/zsh/zshenv
+fi
+
+curl -Ss https://starship.rs/install.sh | sh
+
+echo "Change to a normal user and install OhMyZSH..."
